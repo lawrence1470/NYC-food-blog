cbfa14c17f5b3619cea9ecfaa2e224782cd139a0
lawrence nicastro <lawrence.nicastro1@gmail.com>
2022-11-04 13:58:11 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Fallback from "../components/fallback"
 
 export default function Homepage(props) {
   const { homepage } = props.data
-
+console.log(props,'props')
   return (
     <Layout {...homepage}>
       {homepage.blocks.map((block) => {
